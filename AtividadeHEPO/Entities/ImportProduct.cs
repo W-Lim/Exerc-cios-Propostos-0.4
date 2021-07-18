c1e7012580c2d4f1627dbcc5cc052de2aae2c588
Wender lima <wenderlima678@gmail.com>
2021-07-17 21:32:02 -0300

    Atividade completa sobre hernaça e polimorfismo

---
+++ AtividadeHEPO/Entities/ImportProduct.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Globalization;
+
+namespace AtividadeHEPO.Entities
+{
+    class ImportProduct : Product
+    {
+        public double CustomsFee { get; set; }
+
+        public ImportProduct() { }
+
+        public ImportProduct(string name, double price, double customsFee)
+             : base(name, price)
+        {
+            CustomsFee = customsFee;
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomsFee;
+        }
+
+        public override string Pricetag()
+        {
+            return Name
+                + " $"
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + "(Customs fee: $ "
+                + CustomsFee.ToString("F2", CultureInfo.InvariantCulture) 
+                + ")";
+        }
+    }
+}
---
+++ AtividadeHEPO/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using AtividadeHEPO.Entities;
+
+namespace AtividadeHEPO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Product> list = new List<Product>();
+
+            Console.Write("Enter the number of products:");
+            int qtd = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= qtd; i++)
+            {
+                Console.WriteLine("Product #" + i + " data:");
+                Console.Write("Common, used or imported (c/u/i)? ");
+                string op = Console.ReadLine();
+
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Price: ");
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                if(op == "u")
+                {
+                    Console.Write("Manufacture date (DD/MM/YYYY): ");
+                    DateTime date = DateTime.Parse(Console.ReadLine());
+                    list.Add(new UsedProduct(name, price, date));
+                }
+                else if(op == "i")
+                {
+                    Console.Write("Customs fee: ");
+                    double customs = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                    list.Add(new ImportProduct(name, price, customs));
+                }
+                else
+                {
+                    list.Add(new Product(name, price));
+                }
+            }
+
+            Console.WriteLine(  );
+            Console.WriteLine("PRICE TAGS:");
+            foreach (Product pro in list)
+            {
+                Console.WriteLine(pro.Pricetag());
+            }
+
+            
+        }
+    }
+}
